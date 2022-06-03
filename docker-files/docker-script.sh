0f1b9de9f04f338ebfba8e0f127f335b7d4fc953
uma <abc@xyz.com>
2022-06-02 21:17:56 -0400

    jenkin file

--- docker-files/docker-script.sh
+++ docker-files/docker-script.sh
@@ -2,3 +2,4 @@ sudo yum install docker -y
 sudo systemctl start docker
 sudo curl -Ls "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
